7bed49a9327f425359405ac3b831dafa726cb21d
lalitharajendiran <lalitha.rajendiran@ssomens.com>
2015-06-03 12:23:46 +0530

    Deleted unwanted files,Renamed file name  nd out in corresponding folder.

--- application/controllers/Ctrl_Report_Tickler_History.php
+++ application/controllers/Ctrl_Report_Tickler_History.php
@@ -5,7 +5,7 @@ class Ctrl_Report_Tickler_History extends CI_Controller{
     //FUNCTION FOR INDEX FILE
     public function index()
     {
-        $this->load->view('REPORT/Vw_report_tickler_history');
+        $this->load->view('REPORT/Vw_Report_Tickler_History');
     }
     //FUNCTIONFOR SAVE PART
     public function TH_customername_autocomplete()
@@ -14,14 +14,14 @@ class Ctrl_Report_Tickler_History extends CI_Controller{
         $this->load->model('Common');
         $errorlist= $this->input->post('ErrorList');
         $ErrorMessage= $this->Common->getErrorMessageList($errorlist);
-        $this->load->model('Mdl_report_tickler_history');
+        $this->load->model('REPORT/Mdl_report_tickler_history');
         $result = $this->Mdl_report_tickler_history->customername_autocomplete($USERSTAMP,$ErrorMessage) ;
         echo JSON_encode($result);
     }
     // fetch data
     public function fetchdata()
     {
-        $this->load->model('Mdl_report_tickler_history');
+        $this->load->model('REPORT/Mdl_report_tickler_history');
         $data=$this->Mdl_report_tickler_history->fetch_data();
         echo json_encode($data);
     }
